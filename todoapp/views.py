60cad8658b4d494cdb67efcc40cdc11f4b67ae7f
sirajirasajiki <fujikihirotaka02@gmail.com>
2025-01-02 02:26:15 +0000

    タスク一覧表示のためのビューとテンプレートを実装

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from todoapp.models import Task
 
 # Create your views here.
-def taskList(request):
-    return HttpResponse("<h1>Hello Django<h1>")
+class TaskList(ListView):
+    model = Task
+    
--- todoproject/settings.py
+++ todoproject/settings.py
@@ -37,7 +37,7 @@ ROOT_URLCONF = 'todoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
