cc7704166956a5abfa2eab23c05371acefa57c8b
sirajirasajiki <fujikihirotaka02@gmail.com>
2025-01-02 02:52:57 +0000

    タスク詳細ページの実装

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from todoapp.models import Task
 
@@ -8,3 +9,7 @@ from todoapp.models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
