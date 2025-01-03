25ce22e24e9cc524ecf1c4aef485be4a6e00f2b2
sirajirasajiki <fujikihirotaka02@gmail.com>
2025-01-03 00:39:34 +0000

    スタイルシートを作成し各ページに反映

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -100,6 +100,7 @@ LOGIN_URL = "login"
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
